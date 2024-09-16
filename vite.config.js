507a54412bef7845026a4d8c2ff3219ba72b1cad
JoseA1m <josealmenarez08@gmail.com>
2024-09-16 14:38:14 -0500

    V2.0

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/WEB-FE-HTML-TAILWIND-BONICE/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
