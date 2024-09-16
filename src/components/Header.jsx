1b3052b6e56d0ce3c1ed0889a745e545a743d9c4
JoseA1m <josealmenarez08@gmail.com>
2024-09-16 15:01:51 -0500

    V5.3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header class="bg-[#DBFBFF] p-4 flex justify-center items-center">
-      <img src="https://Orun1tia.github.io/WEB-FE-HTML-TAILWIND-BONICE/logo.png" alt="logo" />
+      <img src="/WEB-FE-HTML-TAILWIND-BONICE/logo.png" alt="logo" />
     </header>
   );
 }
